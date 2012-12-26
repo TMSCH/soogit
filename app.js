044d55cbfc14add7af74338451c7ea5c6b639748
Thomas Césaré-Herriau <thomas.cesareherriau@gmail.com>
2012-12-27 00:05:40 +0100

    initial commit 6

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.post('playlist', api.generatePlaylistFromTracklist);
 app.get('/search-tracks/:query', api.searchTracks)
 app.delete('/tracks/:id', api.deleteTrack);
 
-app.listen(app.get('port'), function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
